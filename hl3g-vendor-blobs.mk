21bb7fb8806c46301b6de104b0df5342eb4b963f
Aman Kumar <amankumarmicro@gmail.com>
2016-02-11 17:29:33 +0530

    add missing lib

--- hl3g-vendor-blobs.mk
+++ hl3g-vendor-blobs.mk
@@ -140,4 +140,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/hl3g/proprietary/lib/libExynosHWCService.so:system/lib/libExynosHWCService.so \
     vendor/samsung/hl3g/proprietary/lib/libexynosscaler.so:system/lib/libexynosscaler.so \
     vendor/samsung/hl3g/proprietary/lib/libymc_codecctrl.so:system/lib/libymc_codecctrl.so \
-    vendor/samsung/hl3g/proprietary/lib/libion.so:system/lib/libion.so
+    vendor/samsung/hl3g/proprietary/lib/libion.so:system/lib/libion.so \
+    vendor/samsung/hl3g/proprietary/lib/libexynosgscaler.so:system/lib/libexynosgscaler.so
